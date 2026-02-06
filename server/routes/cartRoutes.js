47c10936a5b381b59466d5aca5a175a767cc2cee
qasim ahmad <qasimahmad2555@gmail.com>
2026-02-06 19:35:41 +0500

    Implement cart management functionality: add routes and controller methods for retrieving, adding, updating, and removing items in the cart

--- server/routes/cartRoutes.js
+++ server/routes/cartRoutes.js
@@ -1,5 +1,19 @@
 import express from 'express'
+import { protect } from '../middleware/authMiddleware.js'
+import { addItemToCart, clearCart, getCart, removeItemFromCart, updateCartItem } from '../controllers/cartController.js'
 
 const router = express.Router()
 
+router.use(protect)
+
+router.route("/")
+    .get(getCart)
+    .post(addItemToCart)
+    .delete(clearCart)
+
+router.route("/update")
+    .put(updateCartItem)
+
+router.route("/:productId")
+    .delete(removeItemFromCart)
 export default router
