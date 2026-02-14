661235660d416548686311c53debbf2d985f5a17
qasim ahmad <qasimahmad2555@gmail.com>
2026-02-14 21:19:02 +0500

    feat: enhance Register component with form handling, validation, and improved UI elements

--- admin/src/pages/Register.tsx
+++ admin/src/pages/Register.tsx
@@ -1,14 +1,17 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
-import { Form, FormControl, FormField, FormItem, FormLabel } from '@/components/ui/form'
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
 import { registerSchema } from '@/lib/validation'
 import useAuthStore from '@/store/useAuthstore'
 import { motion } from 'motion/react'
 import { useState } from 'react'
 import { useForm } from 'react-hook-form'
-import { useNavigate } from 'react-router'
+import { Link, useNavigate } from 'react-router'
 import z from 'zod'
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+import { UserPlus } from 'lucide-react'
+import { toast } from 'sonner'
 
 function Register() {
   type FormData = z.infer<typeof registerSchema>
@@ -26,6 +29,21 @@ function Register() {
       role: "user"
     }
   })
+
+  const onSubmit = async (data: FormData) => {
+    setIsLoading(true)
+    try {
+      await register(data)
+      naviagte("/login")
+    } catch (error) {
+      console.error("Registration failed", error)
+      toast.error("Failed to Register!")
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+
   return (
 
     <div
@@ -53,7 +71,10 @@ function Register() {
 
           <CardContent>
             <Form {...form}>
-              <form>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-6"
+              >
                 <FormField
                   control={form.control}
                   name="name"
@@ -66,14 +87,97 @@ function Register() {
                           placeholder="John doe"
                           type="text"
                           disabled={isLoading}
+                          className="border focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 placeholder:text-sm hoverEffect"
+                          {...field}
                         />
                       </FormControl>
+                      <FormMessage className="text-red-500 text-xs" />
                     </FormItem>
                   )}
                 />
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="text-sm font-medium text-gray-700">
+                        Email
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="you@exmaple.com"
+                          type="email"
+                          disabled={isLoading}
+                          className="border focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 placeholder:text-sm hoverEffect"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage className="text-red-500 text-xs" />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="text-sm font-medium text-gray-700">
+                        Password
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="********"
+                          type="password"
+                          disabled={isLoading}
+                          className="border focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 placeholder:text-sm hoverEffect"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage className="text-red-500 text-xs" />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="role"
+                  render={() => (
+                    <FormItem>
+                      <FormLabel className="text-sm font-medium text-gray-700">
+                        Role
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="User"
+                          type="text"
+                          disabled={true}
+                          className="border-gray-300 bg-gray-100 text-gray-500 cursor-not-allowed"
+                        />
+                      </FormControl>
+                      <FormMessage className="text-red-500 text-xs" />
+                    </FormItem>
+                  )}
+                />
+
+                <Button
+                  type="submit"
+                  className="w-full bg-indigo-600 hover:bg-indigo-700 hoverEffect text-white font-semibold py-2 rounded-lg"
+                >
+                  <UserPlus /> Sign Up
+                </Button>
               </form>
             </Form>
           </CardContent>
+          <CardFooter className="justify-center">
+            <p className="text-sm text-gray-500">
+              Already have an account?{" "}
+              <Link
+                to={"/login"}
+                className="text-indigo-600 hover:text-indigo-800 hoverEffect hover:underline"
+              >
+                Sign in
+              </Link>
+            </p>
+          </CardFooter>
         </Card>
       </motion.div>
     </div >
