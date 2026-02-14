3ecb937eab89078e445a7dbe59ab16cabe17cf65
qasim ahmad <qasimahmad2555@gmail.com>
2026-02-14 18:40:17 +0500

    feat: implement validation schemas and enhance Register component with form handling

--- admin/src/pages/Register.tsx
+++ admin/src/pages/Register.tsx
@@ -1,11 +1,35 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
-import { Form } from '@/components/ui/form'
+import { Form, FormControl, FormField, FormItem, FormLabel } from '@/components/ui/form'
+import { registerSchema } from '@/lib/validation'
+import useAuthStore from '@/store/useAuthstore'
 import { motion } from 'motion/react'
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { useNavigate } from 'react-router'
+import z from 'zod'
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from '@/components/ui/input'
 
 function Register() {
+  type FormData = z.infer<typeof registerSchema>
+
+  const [isLoading, setIsLoading] = useState(false)
+  const naviagte = useNavigate()
+  const { register } = useAuthStore()
+
+  const form = useForm<FormData>({
+    resolver: zodResolver(registerSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+      role: "user"
+    }
+  })
   return (
+
     <div
-      className="min-h-screen w-full bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500 flex items-center justify-center">
+      className="min-h-screen w-full bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500 flex items-center justify-center" >
       <motion.div
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
@@ -28,16 +52,31 @@ function Register() {
           </CardHeader>
 
           <CardContent>
-            <Form>
-          <form>
-            
-          </form>
+            <Form {...form}>
+              <form>
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) =>
+                  (
+                    <FormItem>
+                      <FormLabel>Name</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="John doe"
+                          type="text"
+                          disabled={isLoading}
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+              </form>
             </Form>
           </CardContent>
-
         </Card>
       </motion.div>
-    </div>
+    </div >
   )
 }
 
