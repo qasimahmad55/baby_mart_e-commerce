e244ae0426923bb9dc2f5a8394f418922fca8de2
Qasim Ahmad <qasimahmad2555@gmail.com>
2026-02-18 22:07:30 +0500

    feat: enhance App, Header, and Sidebar components with user authentication and dynamic sidebar functionality

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -3,11 +3,14 @@ import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import { toast, Toaster } from "sonner";
 import useAuthStore from "./store/useAuthstore";
+import { useState } from "react";
+import { cn } from "./lib/utils";
 // import { Button } from "./components/ui/button";
 
 function App() {
 
   const { isAuthenticated } = useAuthStore()
+  const [sidebarOpen, setSidebarOpen] = useState(true)
   // console.log(isAuthenticated);
   if (!isAuthenticated) {
     return <Navigate to={"/login"} />
@@ -15,16 +18,16 @@ function App() {
 
   return (
     <div className="h-screen flex bg-background">
-      <Sidebar />
+      <Sidebar open={sidebarOpen} setOpen={setSidebarOpen} />
 
-      <div className="flex flex-col flex-1 max-w-[--breakpoint-2xl] ml-64">
+      <div className={cn("flex flex-col flex-1 max-w-[--breakpoint-2xl]", sidebarOpen ? "md:ml-64" : "md:ml-20")}>
         <Header />
         <main>
           <Outlet />
         </main>
       </div>
       <Toaster position="bottom-right" />
-    </div>
+    </div >
   );
 }
 
